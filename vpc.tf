30200e7aa377185886fb2aaa171ff87effca2719
Vijay K <sharmavijay86@gmail.com>
2021-09-15 20:07:31 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,11 +20,17 @@ resource "google_compute_network" "vpc" {
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
+  ip_cidr_range = "10.2.0.0/16"
   region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
+  network       = google_compute_network.vpc.id
   secondary_ip_range {
-    range_name    = "tf-gke-sec-range"
-    ip_cidr_range = "10.10.2.0/24"
+    range_name    = "services-range"
+    ip_cidr_range = "192.168.1.0/24"
+  }
+
+  secondary_ip_range {
+    range_name    = "pod-ranges"
+    ip_cidr_range = "192.168.64.0/22"
   }
 }
+
